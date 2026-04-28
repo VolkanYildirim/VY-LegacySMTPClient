5394aceddcc71db1b8a175671e1573c427d31a09
Volkan Yıldırım <23458532+VolkanYildirim@users.noreply.github.com>
2026-04-28 18:01:10 +0300

    init: Initial commit of legacy SMTP client (authentication refactor required)

--- Gmail İle Mail Gönderme/Program.cs
+++ Gmail İle Mail Gönderme/Program.cs
@@ -15,7 +15,7 @@ namespace Gmail_İle_Mail_Gönderme
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()); 
         }
     }
 }
